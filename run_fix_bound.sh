b4fe0a1c8ce1d3c962a18ddcb25cf25f19999d77
Yifan Yang <cae42@lse88.e-technik.uni-erlangen.de>
2015-01-21 10:24:38 +0100

    new script

--- run_fix_bound.sh
+++ run_fix_bound.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-for i in {5..100..1}
+for i in {5..200..1}
 do
-   t_max=$((20000000/($i*$i*$i)))
+   t_max=$((100000000/($i*$i*$i)))
    echo "N_X = $i, T_MAX = $t_max"
    ./lwacm s $i t $t_max 
 done
+
